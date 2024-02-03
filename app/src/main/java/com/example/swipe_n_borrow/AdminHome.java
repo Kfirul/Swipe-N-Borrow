28798990aaeaad3065ea9d1cfae1ac959a249820
Tomer Gozlan <tomgoz96@gmail.com>
2024-02-04 05:23:58 +0800

    Create Admin UI

--- app/src/main/java/com/example/swipe_n_borrow/AdminHome.java
+++ app/src/main/java/com/example/swipe_n_borrow/AdminHome.java
@@ -4,66 +4,59 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
 
+/**
+ * The {@code UserHome} class represents the main activity for the regular user.
+ * It provides functionality for searching, viewing borrowed books, reissuing books, and logging out.
+ * The user can navigate to different sections of the application by clicking on the corresponding buttons.
+ * Additionally, the user has the ability to log out of the application.
+ */
 public class AdminHome extends AppCompatActivity {
-    ProgressBar progressBar;
-    FirebaseFirestore fstore;
 
-    String adminID;
-    Button searchBook;
-    Button addBook;
-    Button removeBook;
-    Button updateBook;
-    Button issueBook;
-    Button BooksList;
-    Button reissueBook;
-    Button collectFine;
-    Button logOut;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    ViewPagerAdapter2 viewPagerAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        setContentView(R.layout.activity_admin_home2);
 
-        // Initialize buttons
-       searchBook = findViewById(R.id.searchBook);
-       addBook = findViewById(R.id.addBook);
-       removeBook = findViewById(R.id.removeBook);
-       updateBook = findViewById(R.id.updateBook);
-       issueBook = findViewById(R.id.issueBook);
-       BooksList = findViewById(R.id.booksList);
-       reissueBook = findViewById(R.id.reissueBook);
-       collectFine = findViewById(R.id.collect1);
-       logOut = findViewById(R.id.logOut);
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager2=findViewById(R.id.viewpager);
+        viewPagerAdapter=new ViewPagerAdapter2(this);
+        viewPager2.setAdapter(viewPagerAdapter);
 
-        addBook.setOnClickListener(new View.OnClickListener() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AdminAddBook.class);
-                startActivity(intent);
-                finish();
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
 
             }
         });
-
-       logOut.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               FirebaseAuth.getInstance().signOut();
-               Intent intent = new Intent(getApplicationContext(), Login.class);
-               startActivity(intent);
-               finish();
-
-           }
-       });
-
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
-
 
 }
