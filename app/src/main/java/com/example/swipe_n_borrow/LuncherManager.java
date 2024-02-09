75d87f88f2b10489b08d82371e4562f371d27fa3
Tomer Gozlan <tomgoz96@gmail.com>
2024-02-09 02:29:53 +0200

    add splashScreen & fix sliders to return user & show user info home

---
+++ app/src/main/java/com/example/swipe_n_borrow/LuncherManager.java
@@ -0,0 +1,27 @@
+package com.example.swipe_n_borrow;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class LuncherManager {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private static String PREF_NAME = "LunchManger";
+    private static String IS_FIRST_TIME ="isFirst";
+
+    public LuncherManager(Context context){
+        sharedPreferences = context.getSharedPreferences(PREF_NAME,0);
+        editor=sharedPreferences.edit();
+    }
+
+    public void setFirstLunch(boolean isFirst){
+        editor.putBoolean(IS_FIRST_TIME,isFirst);
+        editor.commit();
+    }
+
+    public boolean osFirstTime(){
+        return sharedPreferences.getBoolean(IS_FIRST_TIME,true);
+    }
+
+}
--- app/src/main/java/com/example/swipe_n_borrow/UserHomeFragment.java
+++ app/src/main/java/com/example/swipe_n_borrow/UserHomeFragment.java
@@ -1,10 +1,14 @@
 package com.example.swipe_n_borrow;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +16,15 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +46,7 @@ public class UserHomeFragment extends Fragment {
     private LinearLayout bookListLayout;
     private TextView textViewAddress, textViewFullName, textViewPhoneNumber;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
 
     public UserHomeFragment() {
@@ -68,18 +81,21 @@ public class UserHomeFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        textViewAddress = getActivity().findViewById(R.id.profileAddress);
+        textViewFullName = getActivity().findViewById(R.id.profilelName);
+        textViewPhoneNumber = getActivity().findViewById(R.id.phoneNumber);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user_home, container, false);
 
         // Find the TextView elements
-        textViewAddress = view.findViewById(R.id.profileAddress);
-        textViewFullName = view.findViewById(R.id.profilelName);
-        textViewPhoneNumber = view.findViewById(R.id.phoneNumber);
-
-//        // Fetch and display admin data
-//        showAdminData();
 
         logOut = view.findViewById(R.id.logOut);
 
@@ -103,5 +119,40 @@ public class UserHomeFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentId = user.getUid();
+
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference=firestore.collection("Users").document(currentId);
+
+        reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.getResult().exists()){
+
+                    String nameResult = task.getResult().getString("fullName");
+                    String phoneResult = task.getResult().getString("phoneNumber");
+                    String addressResult = task.getResult().getString("address");
+
+                    textViewAddress.setText(addressResult);
+                    textViewFullName.setText(nameResult);
+                    textViewPhoneNumber.setText(phoneResult);
+
+                }else{
+                    textViewAddress.setText("Null");
+                    textViewFullName.setText("Null");
+                    textViewPhoneNumber.setText("Null");
+                }
+            }
+        });
+
     }
 }
