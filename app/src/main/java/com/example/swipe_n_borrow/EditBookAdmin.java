bca9167eb57f8fd62807a5ac0644a1b8e5b47836
David Stern <davidstern135@gmail.com>
2024-02-11 18:09:39 +0200

    fix editbookadmin

--- app/src/main/java/com/example/swipe_n_borrow/EditBookAdmin.java
+++ app/src/main/java/com/example/swipe_n_borrow/EditBookAdmin.java
@@ -18,7 +18,6 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class EditBookAdmin extends AppCompatActivity {
 
@@ -27,8 +26,6 @@ public class EditBookAdmin extends AppCompatActivity {
     FirebaseFirestore db;
     ProgressBar progressBar;
     String selectedBook = "";
-
-
 
     TextInputEditText titleEditText, authorEditText, languageEditText, numPagesEditText, genreEditText;
 
@@ -58,38 +55,6 @@ public class EditBookAdmin extends AppCompatActivity {
         buttonEdit = findViewById(R.id.BTN_Edit);
         progressBar = findViewById(R.id.progressBar);
 
-        String adminId = mAuth.getCurrentUser().getUid();
-        CollectionReference adminBooksCollection = FirebaseFirestore.getInstance()
-                .collection("Admins")
-                .document(adminId)
-                .collection("books");
-
-        // Query to find the specific book document in the "books" collection
-        adminBooksCollection.whereEqualTo("title", selectedBook)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            // Retrieve book information from Firestore
-                            String title = document.getString("title");
-                            String author = document.getString("authors");
-                            String language = document.getString("language");
-                            String numPages = document.getString("num_pages");
-                            String genre = document.getString("genre");
-
-                            // Set retrieved information to EditText fields
-                            titleEditText.setText(title);
-                            authorEditText.setText(author);
-                            languageEditText.setText(language);
-                            numPagesEditText.setText(numPages);
-                            genreEditText.setText(genre);
-                        }
-                    } else {
-                        // Handle the case where no document is found
-                        Toast.makeText(EditBookAdmin.this, "No document found for the selected book.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
         Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,6 +64,9 @@ public class EditBookAdmin extends AppCompatActivity {
                 finish();
             }
         });
+
+        // Retrieve the book information and populate EditText fields
+        retrieveBookInformation();
 
         buttonEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -177,7 +145,39 @@ public class EditBookAdmin extends AppCompatActivity {
                         });
             }
         });
+    }
 
+    private void retrieveBookInformation() {
+        String adminId = mAuth.getCurrentUser().getUid();
+        CollectionReference adminBooksCollection = FirebaseFirestore.getInstance()
+                .collection("Admins")
+                .document(adminId)
+                .collection("books");
 
+        // Query to find the specific book document in the "books" collection
+        adminBooksCollection.whereEqualTo("title", selectedBook)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Retrieve book information from Firestore
+                            String title = document.getString("title");
+                            String author = document.getString("authors");
+                            String language = document.getString("language");
+//                            String numPages = document.getString("num_pages");
+                            String genre = document.getString("genre");
+
+                            // Set retrieved information to EditText fields
+                            titleEditText.setText(title);
+                            authorEditText.setText(author);
+                            languageEditText.setText(language);
+//                            numPagesEditText.setText(numPages);
+                            genreEditText.setText(genre);
+                        }
+                    } else {
+                        // Handle the case where no document is found
+                        Toast.makeText(EditBookAdmin.this, "No document found for the selected book.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
